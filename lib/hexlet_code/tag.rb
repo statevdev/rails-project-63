4233151714a8ce6fa5dcc78f42eb2ffa2fe4c9f0
statevdev <statev.dev@gmail.com>
2025-01-29 20:06:03 +0600

    wrestling with linter part 2

--- lib/hexlet_code/tag.rb
+++ lib/hexlet_code/tag.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 module HexletCode
+  # Class for generating HTML tags.
   class Tag
-    def self.build(name, attributes = {}, &block)
-      attrs = attributes.map { |key, value| "#{key}=\"#{value}\"" }.join(' ')
-      attrs_string = attrs.empty? ? '' : " #{attrs}"
+    # Generates an HTML tag.
+    def self.build(name, attributes = {}, &_block)
+      attrs = attributes.map { |key, value| "#{key}=\"#{value}\"" }.join(" ")
+      attrs_string = attrs.empty? ? "" : " #{attrs}"
 
       if block_given?
         content = yield
