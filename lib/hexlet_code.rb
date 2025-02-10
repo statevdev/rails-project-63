e591b4b221e8b1fef972336b9fdec22aa67de339
statevdev <statev.dev@gmail.com>
2025-02-10 19:18:08 +0600

    renamed Inputs.user variable to Inputs.object

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -2,26 +2,25 @@
 
 require_relative "hexlet_code/version"
 
-# Главный модуль библиотеки для генерации форм и html-тэгов.
 module HexletCode
   class Error < StandardError; end
 
   autoload :Tag, File.expand_path("hexlet_code/tag.rb", __dir__)
   autoload :Inputs, File.expand_path("hexlet_code/tag.rb", __dir__)
 
-  def self.form_for(user, form_options = {}, &)
+  def self.form_for(object, form_options = {}, &)
     default_form_options = { action: form_options.delete(:url) || "#", method: "post" }
 
     merged_options = default_form_options.merge(form_options)
 
     if block_given?
-      inputs = Inputs.new(user)
+      inputs = Inputs.new(object)
 
       yield(inputs)
 
-      Tag.build(:form, user.to_h, merged_options) { "\n#{inputs.inputs.join("\n")}\n" }
+      Tag.build(:form, object.to_h, merged_options) { "\n#{inputs.inputs.join("\n")}\n" }
     else
-      Tag.build(:form, user.to_h, merged_options, &proc {})
+      Tag.build(:form, object.to_h, merged_options, &proc {})
     end
   end
 end
--- lib/hexlet_code/tag.rb
+++ lib/hexlet_code/tag.rb
@@ -1,27 +1,25 @@
 # frozen_string_literal: true
 
-# Класс для сборки HTML-тэгов.
 class Tag
-  def self.build(tag, _users_hash, user_options = {}, &)
-    tag_attrs_string = user_options.map { |key, value| "#{key}=\"#{value}\"" }.join(" ")
+  def self.build(tag, _objects_hash, object_options = {}, &)
+    tag_attrs_string = object_options.map { |key, value| "#{key}=\"#{value}\"" }.join(" ")
     return "<#{tag} #{tag_attrs_string}>" unless block_given?
 
     "<#{tag} #{tag_attrs_string}>#{yield}</#{tag}>"
   end
 end
 
-# Класс для создания инпутов формы.
 class Inputs
   attr_reader :inputs
 
-  def initialize(user)
-    @user = user
+  def initialize(object)
+    @object = object
     @inputs = []
   end
 
   def input(input_attr, input_attrs = {})
     tag = input_attrs.delete(:as) || :input
-    input = Inputs.send(tag, @user, input_attr, input_attrs)
+    input = Inputs.send(tag, @object, input_attr, input_attrs)
     inputs << input
     input
   end
@@ -33,25 +31,25 @@ class Inputs
     submit
   end
 
-  def self.input(user, input_attr, input_attrs)
-    default_input_attrs = { name: input_attr, type: "text", value: user.public_send(input_attr) }
+  def self.input(object, input_attr, input_attrs)
+    default_input_attrs = { name: input_attr, type: "text", value: object.public_send(input_attr) }
     merged_attrs = default_input_attrs.merge(input_attrs)
 
-    label = self.label(user, input_attr)
-    "#{label}\n#{Tag.build(:input, user.to_h, merged_attrs)}"
+    label = self.label(object, input_attr)
+    "#{label}\n#{Tag.build(:input, object.to_h, merged_attrs)}"
   end
 
-  def self.text(user, input_attr, input_attrs)
+  def self.text(object, input_attr, input_attrs)
     default_input_attrs = { name: input_attr, cols: 20, rows: 40 }
     merged_attrs = default_input_attrs.merge(input_attrs)
 
-    Tag.build(:textarea, user.to_h, merged_attrs) { user.public_send(input_attr) }
+    Tag.build(:textarea, object.to_h, merged_attrs) { object.public_send(input_attr) }
   end
 
-  def self.label(user, input_attr, input_attrs = {})
+  def self.label(object, input_attr, input_attrs = {})
     default_input_attrs = { for: input_attr }
     merged_attrs = default_input_attrs.merge(input_attrs)
 
-    Tag.build(:label, user.to_h, merged_attrs) { input_attr.to_s.capitalize }
+    Tag.build(:label, object.to_h, merged_attrs) { input_attr.to_s.capitalize }
   end
 end
