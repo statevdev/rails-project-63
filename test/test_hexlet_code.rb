c36f6a108367c7d6517f5971ae35ce90df6b7821
statevdev <statev.dev@gmail.com>
2025-02-14 04:19:06 +0600

    changed tests & fixtures

--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -13,48 +13,50 @@ class TestHexletCode < Minitest::Test
   end
 
   def test_form_for_without_inputs
-    fixture = File.read('test/fixtures/fixture_without_inputs.html').chomp
+    fixture = File.read('test/fixtures/fixture_without_inputs.html')
     test = HexletCode.form_for(@user)
-    assert { test == fixture }
+    assert_equal test, fixture
   end
 
   def test_form_for_new_attr
-    fixture = File.read('test/fixtures/fixture_with_new_attr.html').chomp
+    fixture = File.read('test/fixtures/fixture_with_new_attr.html')
     test = HexletCode.form_for(@user, class: 'hexlet-form')
-    assert { test == fixture }
+    assert_equal test, fixture
   end
 
   def test_form_for_reassign_attr
-    fixture = File.read('test/fixtures/fixture_with_reassign_attr.html').chomp
+    fixture = File.read('test/fixtures/fixture_with_reassign_attr.html')
     test = HexletCode.form_for(@user, url: '/profile', class: 'hexlet-form')
-    assert { test == fixture }
+    assert_equal test, fixture
   end
 
   def test_form_for_inputs
-    fixture = File.read('test/fixtures/fixture_with_inputs.html').chomp
+    fixture = File.read('test/fixtures/fixture_with_inputs.html')
     test = HexletCode.form_for @user do |f|
       f.input :name
       f.input :job, as: :text
       f.input :name, class: 'user-input'
       f.input :job, as: :text, rows: 50, cols: 50
     end
-    assert { test == fixture }
+    assert_equal test, fixture
   end
 
   def test_form_for_submit
-    fixture = File.read('test/fixtures/fixture_with_submit.html').chomp
+    fixture = File.read('test/fixtures/fixture_with_submit.html')
     test = HexletCode.form_for @user, url: '#' do |f|
       f.input :name
       f.input :job
       f.submit
       f.submit 'Wow'
     end
-    assert { test == fixture }
+    assert_equal test, fixture
   end
 
   def test_form_for_no_method
-    HexletCode.form_for @user do |f|
-      assert_raises(NoMethodError) { f.input :age }
+    assert_raises(NoMethodError) do
+      HexletCode.form_for @user do |f|
+        f.input :age
+      end
     end
   end
 end
