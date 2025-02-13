81f11eb6a1b28cd9d09d8d3cdd1d78b15c43f38e
statevdev <statev.dev@gmail.com>
2025-02-14 04:15:27 +0600

    realized rendering logic

--- lib/hexlet_code/form_renderer.rb
+++ lib/hexlet_code/form_renderer.rb
@@ -2,8 +2,12 @@
 
 module HexletCode
   class FormRenderer
-    def self.to_html(tag, object, input_attr, input_attrs)
-      HTMLTemplates.send(tag, object, input_attr, input_attrs)
+    def self.render_html(form_body)
+      inputs = "\n"
+      form_body[:inputs].each do |input|
+        inputs += HTMLTemplates.send(input[0], input[1], input[2], input[3]).to_s
+      end
+      Tag.build(:form, form_body[:form_options]) { inputs }
     end
   end
 end
