260b93466078803baad6ec1cdfe5e385068c02d5
statevdev <statev.dev@gmail.com>
2025-02-14 04:17:52 +0600

    added method submit, refactoring

--- lib/hexlet_code/html_templates.rb
+++ lib/hexlet_code/html_templates.rb
@@ -1,32 +1,36 @@
 # frozen_string_literal: true
 
 module HexletCode
-  autoload :Tag, File.expand_path('tag.rb', __dir__)
-
   class HTMLTemplates
-    def self.input(object, input_attr, input_attrs)
-      default_input_attrs = { name: input_attr, type: 'text', value: object.public_send(input_attr) }
+    def self.input(entity, input_attr, input_attrs)
+      default_input_attrs = { name: input_attr, type: 'text', value: entity.public_send(input_attr) }
       merged_attrs = default_input_attrs.merge(input_attrs)
 
-      label = self.label(object, input_attr)
-      input = Tag.build(:input, object.to_h, merged_attrs)
-      "#{label}\n#{input}"
+      label = self.label(entity, input_attr)
+      input = Tag.build(:input, merged_attrs)
+      "#{label}#{input}"
     end
 
-    def self.text(object, input_attr, input_attrs)
+    def self.text(entity, input_attr, input_attrs)
       default_input_attrs = { name: input_attr, cols: 20, rows: 40 }
       merged_attrs = default_input_attrs.merge(input_attrs)
 
-      label = self.label(object, input_attr)
-      textarea = Tag.build(:textarea, object.to_h, merged_attrs) { object.public_send(input_attr) }
-      "#{label}\n#{textarea}"
+      label = self.label(entity, input_attr)
+      textarea = Tag.build(:textarea, merged_attrs) { entity.public_send(input_attr) }
+      "#{label}#{textarea}"
     end
 
-    def self.label(object, input_attr, input_attrs = {})
+    def self.label(_entity, input_attr, input_attrs = {})
       default_input_attrs = { for: input_attr }
       merged_attrs = default_input_attrs.merge(input_attrs)
 
-      Tag.build(:label, object.to_h, merged_attrs) { input_attr.to_s.capitalize }
+      Tag.build(:label, merged_attrs) { input_attr.to_s.capitalize }
+    end
+
+    def self.submit(_entity, _input_attr, text_for_button)
+      submit_attrs = { type: 'submit', value: text_for_button }
+
+      Tag.build(:input, submit_attrs)
     end
   end
 end
